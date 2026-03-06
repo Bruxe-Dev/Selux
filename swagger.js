57d3053b066f06b9eac0b5f3261cb4d11115fed7
Bruxe-Dev <mnibeza23@gmail.com>
2026-03-06 20:26:18 +0200

    sstarted creating server swagger configuration

---
+++ swagger.js
@@ -0,0 +1,22 @@
+import { version } from "mongoose";
+import swaggerJSDoc from "swagger-jsdoc";
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: "Selux Documentation",
+            version: '1.0.0',
+            description: "This is a simple Seller system that will allow sellers to monitor thie products"
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+                description: "This is a development server"
+            }, {
+                url: "https://selux_app.vercel.app",
+                description: "This is a deployment server"
+            }
+        ]
+    }
+}
