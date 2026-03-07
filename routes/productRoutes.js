cf3af9578241b8136b23f7b76b279a402d179883
Bruxe-Dev <mnibeza23@gmail.com>
2026-03-07 06:35:24 +0200

    finished the swagger documentation of searching product by name

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -17,35 +17,58 @@ router.get('/', productController.getProducts)
 
 /**
  * @swagger
- * /api/products:
- *  post:
- *      summary: Creates a new Product
- *      description: This api will help sellers create new products that they have purchased
- *      requestBody:
- *          required: true
- *          content:
- *              application/json:
- *                  schema:
- *                      type: object
- *                  properties:
- *                      name:
- *                          type: string
- *                          example: Laptop
- *                      description:
- *                          type: string
- *                          example: These are sample shoes 
- *                      price:
- *                          type: number
- *                          example: 1200
- *                      quantity:
- *                          type: number
- *                          example: 5
- *      responses:
- *          201:
- *              description: A new Product created
+ * /api/products/search:
+ *  get:
+ *      summary:Finds products by name
+ *      description: This api is to help people find specific product by thier names
+ *      parameters:
+ *          - in: query
+ *          name:name
+ *          required:false
+ *          description: Product name to search
+ *          schema:
+ *              type:string
+ *              example: laptop
+ * 
+ *       responses:
+ *          200:
+ *              description: Products found
+ *          404:
+ *              description: Product not found
  */
+router.get('/search/:name', productController.getProductByName)
+/**
+* @swagger
+* /api/products:
+*  post:
+*      summary: Creates a new Product
+*      description: This api will help sellers create new products that they have purchased
+*      requestBody:
+*          required: true
+*          content:
+*              application/json:
+*                  schema:
+*                      type: object
+*                  properties:
+*                      name:
+*                          type: string
+*                          example: Laptop
+*                      description:
+*                          type: string
+*                          example: These are sample shoes 
+*                      price:
+*                          type: number
+*                          example: 1200
+*                      quantity:
+*                          type: number
+*                          example: 5
+*      responses:
+*          201:
+*              description: A new Product created
+*/
+
+router.get('/search/:id', productController.getProduct)
 router.post('/', productController.createProduct)
-router.get('/:id', productController.getProduct)
 router.patch('/:id', productController.updateProduct)
 router.delete('/:id', productController.deleteProduct)
 
