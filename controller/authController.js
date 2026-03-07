7bda3a4a37d879c6ba2a03fc670b056ae08dd282
Bruxe-Dev <mnibeza23@gmail.com>
2026-03-07 17:03:07 +0200

    created a registere controller

--- controller/authController.js
+++ controller/authController.js
@@ -1,4 +1,22 @@
 import User from '../models/User'
 import bcrypt from 'bcryptjs'
 import jsonwebtoken from 'jsonwebtoken'
+import { use } from 'react'
 
+export const register = async (req, res) => {
+    const { name, email, password } = req.body
+
+    const salt = await bcrypt.genSalt(15)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const user = await User.create({
+        name: name,
+        email: email,
+        password: hashedPassword
+    })
+
+    res.status(201).json({
+        message: "User createsd sucessfuly",
+        user
+    })
+}
