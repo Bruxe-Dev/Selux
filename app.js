7a94d71f19000e424b4debeae11a7d361abc6034
Bruxe-Dev <mnibeza23@gmail.com>
2026-03-16 20:34:30 +0200

    imported the order routes in the app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import productRoutes from './routes/productRoutes.js'
-
+import orderRoutes from './routes/orderRoutes.js'
 const app = express()
 
 app.use(express.json())
 
 app.use("/api/products", productRoutes)
+application.use('/api/order', orderRoutes)
 
 export default app
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,3 +2,6 @@ import * as orderController from '../controller/orderController.js'
 import express from 'express'
 import { authenticate } from '../middleware/authMiddleware.js'
 
+const router = express.Router()
+
+router.post('/', authenticate, orderController.createOrder)
