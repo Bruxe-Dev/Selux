28d5f33920903230a86f4ba0b3e6eed1494b3f82
Bruxe-Dev <mnibeza23@gmail.com>
2026-03-16 20:09:29 +0200

    Created an order model

---
+++ models/Order.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose"
+
+const orderSchema = new mongoose.Schema({
+
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true
+    },
+
+    client: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    quantity: {
+        type: Number,
+        required: true
+    },
+
+    totalPrice: {
+        type: Number,
+        required: true
+    },
+
+    status: {
+        type: String,
+        enum: ["pending", "confirmed", "shipped", "delivered"],
+        default: "pending"
+    }
+
+}, { timestamps: true })
+
+export default mongoose.model("Order", orderSchema)
