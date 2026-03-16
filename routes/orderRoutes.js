e34b90f480bef478e0b23037d6f1cc443a4759fc
Bruxe-Dev <mnibeza23@gmail.com>
2026-03-16 20:57:09 +0200

    created the Swagger Documentation of the create order route

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,6 +4,32 @@ import { authenticate } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
+/**
+ * @swagger
+ * /api/order:
+ * post:
+ *  summary: Create an order
+ *  description: The customer creates an order from ones store
+ *  requestBody:
+ *      required: true
+ *      content:
+ *          application/json:
+ *              schema:
+ *                  type: object
+ *                  required: true:
+ *                        - productId
+ *                        - quantity
+ *                  properties:
+ *                      productId:
+ *                          type:string
+ *                          example:1e2342d3rft25d4
+ *                      quantity:
+ *                          type: number
+ *                          example: 10
+ *      responses:
+ *          201:
+ *              description: Order created Successfully
+ */
 router.post('/', authenticate, orderController.createOrder)
 router.get('/my-orders', authenticate, orderController.getMyOrders)
 
