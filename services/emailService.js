ce744857e4ee84ccdbddfe64a6e312503b3dcf1f
Bruxe-Dev <mnibeza23@gmail.com>
2026-03-21 12:32:55 +0200

    finished up the email service

--- services/emailService.js
+++ services/emailService.js
@@ -8,3 +8,19 @@ const transporter = nodemailer.createTransport({
         pass: config.email_pass
     }
 })
+
+export const sendEmail = async (to, subject, html) => {
+    try {
+        await transporter.sendMail({
+            from: `"Selux App" < ${config.email_user}>`,
+            to,
+            subject,
+            html
+        })
+
+        console.log("Email Sent Successfully")
+
+    } catch (error) {
+        console.log('Failed to sned Email', error)
+    }
+}
