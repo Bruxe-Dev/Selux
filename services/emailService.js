15fe1f54bc313a1916d3dac49fe2086f4f7d2acb
Bruxe-Dev <mnibeza23@gmail.com>
2026-03-21 12:27:04 +0200

    created the fundation of the email service file

--- services/emailService.js
+++ services/emailService.js
@@ -1,2 +1,10 @@
 import nodemailer from 'nodemailer'
+import config from '../config.js'
 
+const transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: config.email_user,
+        pass: config.email_pass
+    }
+})
