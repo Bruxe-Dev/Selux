9fac2732f6314d510f532cda12ed2f2e0f52b0e7
Bruxe-Dev <mnibeza23@gmail.com>
2026-03-21 13:29:59 +0200

    Debugged the Sellers model

--- models/Seller.js
+++ models/Seller.js
@@ -26,33 +26,3 @@ const sellerSchema = new mongoose.Schema({
 })
 
 export default mongoose.model("Seller", sellerSchema)
-return
-    }
-
-const salt = await bcrypt.genSalt(12)
-this.password = bcrypt.hash(this.password, salt)
-})
-
-sellerSchema.methods.matchPassword = async function (inputPassword) {
-    return await bcrypt.compare(inputPassword, this.password)
-}
-
-userSchema.methods.getSignedJwtToken = function () {
-    const secret = process.env.JWT_SECRET;
-    const expire = process.env.JWT_EXPIRE;
-
-    if (!secret) {
-        throw new Error('JWT_SECRET is required');
-    }
-
-    const options = {
-        expiresIn: expire
-    };
-
-    return jwt.sign(
-        { id: this._id.toString() },
-        secret,
-        options
-    );
-};
-const Seller = mongoose.model('Seller', sellerSchema)
