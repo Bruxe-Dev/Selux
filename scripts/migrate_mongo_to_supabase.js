7f44ab37dd84a1c7a8edd39d33411e069af2a5c2
Bruxe-Dev <mnibeza23@gmail.com>
2026-03-26 17:35:28 +0200

    finished adding up all the imports in the migrate file

--- scripts/migrate_mongo_to_supabase.js
+++ scripts/migrate_mongo_to_supabase.js
@@ -1,2 +1,6 @@
 import mongoose from "mongoose";
 import supabase from '../config/supabase.js'
+import config from '../config.js'
+import Product from "../models/Product.js";
+import Order from '../models/Order.js';
+import User from '../models/User.js';
