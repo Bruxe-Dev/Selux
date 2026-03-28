06aa331d3ffe2a7e62a7d9f5835de9ee6ced7f5a
Bruxe-Dev <mnibeza23@gmail.com>
2026-03-28 10:18:03 +0400

    finished the upload service

--- services/uploadService.js
+++ services/uploadService.js
@@ -1,2 +1,24 @@
-import supabase from "../config/database.js";
+// services/uploadService.js
+import supabase from '../config/database.js'
 import { v4 as uuidv4 } from 'uuid'
+
+export const uploadProductImage = async (fileBuffer, mimetype) => {
+    const extension = mimetype.split('/')[1]  // e.g. 'jpeg', 'png'
+    const fileName = `${uuidv4()}.${extension}` // unique name e.g. abc-123.png
+
+    const { data, error } = await supabase.storage
+        .from('product-images')
+        .upload(fileName, fileBuffer, {
+            contentType: mimetype,
+            upsert: false
+        })
+
+    if (error) throw error
+
+    // Get the public URL
+    const { data: urlData } = supabase.storage
+        .from('product-images')
+        .getPublicUrl(fileName)
+
+    return urlData.publicUrl
+}
